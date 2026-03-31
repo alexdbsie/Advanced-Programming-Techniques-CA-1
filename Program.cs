0d15e18589fa9bb616acd212ae690e3eb452eb17
alexdbsie <20093197@mydbs.ie>
2026-03-31 11:43:04 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Class to represent a Churros menu item
+class Churros
+{
+    public string Flavour { get; private set; }
+    public double Price { get; private set; }
+
+    // Constructor
+    public Churros(string flavour, double price)
+    {
+        Flavour = flavour;
+        Price = price;
+    }
+}
