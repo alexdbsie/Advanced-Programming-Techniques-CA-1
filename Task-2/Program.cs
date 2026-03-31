7bb47f6d143a1862f65ecbd0415ebce44dc76638
alexdbsie <20093197@mydbs.ie>
2026-03-31 13:23:46 +0100

    Initial commit

---
+++ Task-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Element
+{
+    public int AtomicNumber;
+    public string Name;
+    public string Class;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program started...");
+    }
+}
