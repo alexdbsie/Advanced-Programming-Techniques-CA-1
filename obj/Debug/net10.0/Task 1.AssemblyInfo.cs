ce24f404c8457b9d4008e8c7eb57ccf79ff3a553
alexdbsie <20093197@mydbs.ie>
2026-03-31 12:09:42 +0100

    Main Program code with unit testing

--- obj/Debug/net10.0/Task 1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d15e18589fa9bb616acd212ae690e3eb452eb17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ded771a873e0868771b4415e71612cb84b1ae55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
