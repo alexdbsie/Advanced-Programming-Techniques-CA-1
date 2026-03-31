5ded771a873e0868771b4415e71612cb84b1ae55
alexdbsie <20093197@mydbs.ie>
2026-03-31 11:54:11 +0100

    Added Class Order

--- Program.cs
+++ Program.cs
@@ -14,3 +14,46 @@ class Churros
         Price = price;
     }
 }
+
+// Class to represent a customer order
+class Order
+{
+    private static int nextOrderNo = 1;
+
+    public int OrderNo { get; private set; }
+    public string OrderDetails { get; private set; }
+    public int Quantity { get; private set; }
+    public double Bill { get; private set; }
+
+    // Constructor
+    public Order(string orderDetails, int quantity)
+    {
+        OrderNo = nextOrderNo;
+        nextOrderNo = nextOrderNo + 1;
+        OrderDetails = orderDetails;
+        Quantity = quantity;
+        Bill = 0;
+    }
+
+    // Method to place the order
+    public void place_order()
+    {
+        Console.WriteLine("Order number: " + OrderNo);
+        Console.WriteLine("Item: " + OrderDetails);
+        Console.WriteLine("Quantity: " + Quantity);
+    }
+
+    // Method to calculate and return the total bill
+    public double pay_bill(double price)
+    {
+        Bill = price * Quantity;
+        Console.WriteLine("Total bill: " + Bill + " euro");
+        return Bill;
+    }
+
+    // Method to collect the order
+    public void collect_order()
+    {
+        Console.WriteLine("Order number " + OrderNo + " is ready. Enjoy!");
+    }
+}
