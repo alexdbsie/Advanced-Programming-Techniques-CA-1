bb3bb8a1554aae30ee479783fdebba9316581fd0
alexdbsie <20093197@mydbs.ie>
2026-04-01 21:30:58 +0100

    Client sent customer data to server using TCP

--- client.py
+++ client.py
@@ -7,9 +7,15 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((HOST, PORT))
 
-message = "Hello Server"
-client_socket.send(message.encode())
+name = input("Enter Name: ")
+address = input("Enter Address: ")
+pps = input("Enter PPS Number: ")
+license_file = input("Enter License File Name: ")
 
-print("Message sent to server")
+data = name + "," + address + "," + pps + "," + license_file
+
+client_socket.send(data.encode())
+
+print("Customer data sent to server")
 
 client_socket.close()
