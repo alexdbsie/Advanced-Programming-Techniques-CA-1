80f0418cae515effcf3caa72ba6024d12cede09d
alexdbsie <20093197@mydbs.ie>
2026-04-01 22:20:29 +0100

    Final: added database storage, confirmation message, and registration ID

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import sqlite3
 
 HOST = "127.0.0.1"
 PORT = 65432
@@ -6,6 +7,10 @@ PORT = 65432
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
+conn_db = sqlite3.connect("customers.db")
+cursor = conn_db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS customers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, pps TEXT, license TEXT)""")
 
 print("Server started... Waiting for client")
 
@@ -15,6 +20,21 @@ print("Connected:", addr)
 data = conn.recv(1024)
 message = data.decode()
 print("Received from client:", message)
+
+
+name, address, pps, license_file = message.split(",")
+
+
+cursor.execute("INSERT INTO customers (name, address, pps, license) VALUES (?, ?, ?, ?)", (name, address, pps, license_file))
+
+conn_db.commit()
+
+
+reg_id = cursor.lastrowid
+print("Generated Registration ID:", reg_id)
+
+
+conn.send(str(reg_id).encode())
 
 conn.close()
 server_socket.close()
